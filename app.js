4c4852026c8ef39152988fa4d9e8e17dce1d3785
Patrick Hogan <phogan110@live.com>
2018-11-15 06:41:29 -0700

    created `changeVal` function that multiplies gNum * 4; created 2 sec timeout for `changeVal`

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let getVal = ((value) => {
     value = Math.floor(Math.random() * 10 + 1);
-    console.log('The value for the global number has been stored!')
-    return value
+    console.log('The value for the global number has been stored!');
+    return value;
 });//this returns a number between 1-10 and logs statement to console.
 
 let gNum = getVal();//this assigns return value from getVal to gNum.
+
+let changeVal = ((value) => {
+    value = gNum * 4;
+    console.log(value);
+});
+
+setTimeout(() => {
+    changeVal();
+}, 2000);
