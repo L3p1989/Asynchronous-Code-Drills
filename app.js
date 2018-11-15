5b54812e5ddad97ad4081bfb13fa73e72ae09df5
Patrick Hogan <phogan110@live.com>
2018-11-15 06:23:12 -0700

    created comments

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let getVal = ((value) => {
     value = Math.floor(Math.random() * 10 + 1);
     console.log('The value for the global number has been stored!')
     return value
-});//this returns a number between 
+});//this returns a number between 1-10 and logs statement to console.
 
-let gNum = getVal();
+let gNum = getVal();//this assigns return value from getVal to gNum.
